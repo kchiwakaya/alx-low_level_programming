5bd2e1b50090be3bf06281a38ed2a98dee8e3398
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-07-06 21:48:26 +0200

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,10 +5,10 @@
  * @n: number to convert into binary
  *
  */
-void print_binary(unsigned long int n) 
+void print_binary(unsigned long int n)
 {
   if (n > 1)
     print_binary(n >> 1);
-  
+
   _putchar((n & 1) + '0');
 }
