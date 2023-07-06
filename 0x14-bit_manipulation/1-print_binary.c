880d12439f0153bed85f30d81749d95258356f09
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-07-06 21:41:50 +0200

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * print_binary - function that prints the binary representation of a number.
- * @n: number to convert into binary representation
+ * print_binary - prints the binary representation of a number.
+ * @n: number to convert into binary
  *
  */
 
-void print_binary(unsigned long int n) {
-  if (n == 0) {
+void print_binary(unsigned long int n) 
+{
+  if (n == 0)
+  {
     _putchar('0');
     return;
   }
