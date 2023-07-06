8110c80d8ef96e046268ce74a4ea2db8ac0c0a35
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-07-06 21:39:22 +0200

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,10 +8,10 @@
 
 void print_binary(unsigned long int n) {
   if (n == 0) {
-    printf("0");
+    _putchar('0');
     return;
   }
 
   print_binary(n >> 1);
-  printf("%d", n & 1);
+  _putchar((n & 1) + '0');
 }
