bac0704bfb9b1034e93752a2c15a1fc45bf001f4
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-07-10 22:11:50 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,40 +1,40 @@
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * create_file - create a file
- * @filename: name of the file to create
- * @text_content: text to insert in the file
- *
- * Return: 1 on success, -1 on failure
+ * create_file - creates a file
+ * @filename: name of file be created
+ * @text_content: text to put in file
+ * Return: 1 on success
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd = 0, fdw = 0, count = 0;
+	int file1 = 0;
+	int file2 = 0;
+	int holder = 0;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	file1 = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file1 == -1)
 		return (-1);
 
 	if (text_content == NULL)
 		text_content = "";
-	while (text_content[count] != '\0')
-		count++;
+	while (text_content[holder] != '\0')
+		holder = holder + 1;
 
-	fdw = write(fd, text_content, count);
-	if (fdw == -1)
+	file2 = write(file1, text_content, holder);
+	if (file2 == -1)
 	{
-		close(fd);
+		close(file1);
 		return (-1);
 	}
-	close(fd);
+	close(file1);
 	return (1);
 }
