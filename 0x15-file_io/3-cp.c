723d23f6d86a19ed9c3e90c76b3efb6a79dd9143
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-07-12 07:12:53 +0200

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,29 +6,29 @@
 
 #define BUFFER_SIZE 1024
 
+char buffer[BUFFER_SIZE];
+int bytes_read;
+char *file_from = argv[1];
+char *file_to = argv[2];
+
+int fd_to = open(file_to, O_WRONLY | O_TRUNC | O_CREAT, 0664);
+int fd_from = open(file_from, O_RDONLY);
+
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     dprintf(2, "Usage: cp file_from file_to\n");
     exit(97);
   }
 
-  char *file_from = argv[1];
-  char *file_to = argv[2];
-
-  int fd_from = open(file_from, O_RDONLY);
   if (fd_from < 0) {
     dprintf(2, "Error: Can't read from file %s\n", file_from);
     exit(98);
   }
 
-  int fd_to = open(file_to, O_WRONLY | O_TRUNC | O_CREAT, 0664);
   if (fd_to < 0) {
     dprintf(2, "Error: Can't write to file %s\n", file_to);
     exit(99);
   }
-
-  char buffer[BUFFER_SIZE];
-  int bytes_read;
 
   while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0) {
     write(fd_to, buffer, bytes_read);
