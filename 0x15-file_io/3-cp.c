aba44ad4544e7719c9e29e6d0cb8ea6ebaa313b8
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-07-14 16:15:51 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,34 +14,38 @@ char *file_to;
 int fd_to;
 int fd_from;
 
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
+int main(int argc, char *argv[])
+{
+  if (argc != 2) 
+  {
     dprintf(2, "Usage: cp file_from file_to\n");
     exit(97);
   }
-
-  if (fd_from < 0) {
+  
+  if (fd_from < 0) 
+  {
     dprintf(2, "Error: Can't read from file %s\n", file_from);
     exit(98);
   }
   file_from = argv[1];
   file_to = argv[2];
-
+  
   fd_to = open(file_to, O_WRONLY | O_TRUNC | O_CREAT, 0664);
   fd_from = open(file_from, O_RDONLY);
-
-  if (fd_to < 0) {
+  
+  if (fd_to < 0)
+  {
     dprintf(2, "Error: Can't write to file %s\n", file_to);
     exit(99);
   }
-
-  while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0) {
+  
+  while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0) 
+  {
     write(fd_to, buffer, bytes_read);
   }
-
+  
   close(fd_from);
   close(fd_to);
-
   return 0;
 }
 
